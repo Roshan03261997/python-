4aa28193cbb849618067db056fb27d8879c32375
Roshan03261997 <68584616+Roshan03261997@users.noreply.github.com>
2020-07-21 20:41:54 +0530

    Add files via upload

---
+++ DAY2ASSIGN.py
@@ -0,0 +1,22 @@
+print ("Hello, World!")
+print ("hello\n world")
+print ("hello\t world")
+print ("""hello""world""")
+
+print("The name of the person : Roshan, mark : 90, age: 23")
+x = 10 
+print(x)
+str = "assignment operator"
+print ("a" not in str)
+a = 10 
+b = 10
+print (a is b)
+A = 20 
+B = 257
+print (A is not B)
+print (a and b)
+print (a & b)
+print (A | B)
+A = A+B
+print (A!=B)
+print (a==b)
